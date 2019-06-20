3dd14077bdbe881854423a73f7c27fd8a1daf60e
Morgana Spake <morgana.spake@gmail.com>
2019-06-19 22:23:52 -0700

    name and count

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      name: '',
+    };
+  }
+
+  handleChange = (event) => {
+    const name = event.target.value;
+    this.setState({ name });
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    const count = this.state.count + 1;
+    this.setState({ count });
+  }
+
   render() {
     return (
       <React.Fragment>
         <div>
-          <div>Name: </div>
-          <div># of Updates: </div>
+          <div>Name: { this.state.name } </div>
+          <div># of Updates: {this.state.count } </div>
+          <form>
+            <input type="text" onChange={this.handleChange}></input>
+            <button onClick={ this.handleClick }>Submit</button>
+          </form>
         </div>
       </React.Fragment>
     );

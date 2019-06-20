f3f0e89bf45348f2955a577c4560c47e7e5f8afb
Morgana Spake <morgana.spake@gmail.com>
2019-06-19 22:46:20 -0700

    refactored

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from './form';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+        <Form />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 import './index.css';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      name: '',
-    };
-  }
-
-  handleChange = (event) => {
-    const name = event.target.value;
-    this.setState({ name });
-  }
-
-  handleClick = (event) => {
-    event.preventDefault();
-    const count = this.state.count + 1;
-    this.setState({ count });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <div>
-          <div>Name: { this.state.name } </div>
-          <div># of Updates: {this.state.count } </div>
-          <form>
-            <input type="text" onChange={this.handleChange}></input>
-            <button onClick={ this.handleClick }>Submit</button>
-          </form>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
